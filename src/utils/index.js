c3658c18f975be3bb8cbd2080fe921968a851ca0
om patel <om28patel2004@gmail.com>
2023-11-10 00:10:02 +0530

    feat: login and signUp Page

--- src/utils/index.js
+++ src/utils/index.js
@@ -128,22 +128,22 @@ export const registrationFormControls = [
 //   },
 // ];
 
-// export const loginFormControls = [
-//   {
-//     id: "email",
-//     type: "email",
-//     placeholder: "Enter your email",
-//     label: "Email",
-//     componentType: "input",
-//   },
-//   {
-//     id: "password",
-//     type: "password",
-//     placeholder: "Enter your password",
-//     label: "Password",
-//     componentType: "input",
-//   },
-// ];
+export const loginFormControls = [
+  {
+    id: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    id: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+    componentType: "input",
+  },
+];
 
 // export const adminAddProductformControls = [
 //   {
